1d6f85c104d2d3d493f74d110fc3dd3cc2f5aedd
Daniel <pigos487@gmail.com>
2022-03-27 21:06:28 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import countries from "./info/countriesAll.json";
+import "./App.css";
+function formatNumber(num) {
+  return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+}
+const RenderCountries = (prop) => {
+  return prop.countries.map((country, i) => {
+    return (
+      <div key={i}>
+        <img alt={country.name} src={country.flag}></img>
+        <h3>{country.name}</h3>
+        <p>Population: {formatNumber(country.population)}</p>
+        <p>Region: {country.region}</p>
+        <p>Capital:{country.capital} </p>
+      </div>
+    );
+  });
+};
+
+function App() {
+  const filterCountries = (typedValue) => {
+    return countries.filter((country) => {
+      return (
+        country.name.toLowerCase().includes(typedValue.toLowerCase()) ||
+        country.capital?.toLowerCase().includes(typedValue.toLowerCase())
+      );
+    });
+  };
+
+  const [arrayOfCountries, setArrayOfCountries] = useState(countries);
+  return (
+    <div className="App">
+      <input
+        type="text"
+        onChange={(e) => {
+          console.log(e.target.value, filterCountries(e.target.value));
+          setArrayOfCountries(filterCountries(e.target.value));
+        }}
+      ></input>
+      <RenderCountries countries={arrayOfCountries} />
+    </div>
+  );
+}
+
+export default App;
